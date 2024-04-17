6bf8b55e3a054ab7f6d4a6c11607160d36e9795c
John-Thailand <naoki.honda@mirrorfit.jp>
2024-04-17 16:13:04 +0900

    プロジェクト全体のコンパイル

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-var userInput;
-function generateError(message, code) {
-    throw { message: message, errorCode: code };
-}
-var result = generateError('エラーが発生しました', 500);
-console.log(result);
+"use strict";
+const userName = 'Maximilian';
+console.log(userName);
--- app.ts
+++ app.ts
@@ -1,8 +1,3 @@
-let userInput: unknown;
+const userName = 'Maximilian';
 
-function generateError(message: string, code: number): never {
-    throw { message: message, errorCode: code };
-}
-
-const result = generateError('エラーが発生しました', 500);
-console.log(result);
+console.log(userName);
