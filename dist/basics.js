58246bf88e15fef1fa3a56bab1de5a20dcc56ca6
John-Thailand <naoki.honda@mirrorfit.jp>
2024-04-17 17:02:01 +0900

    rootDir/outDirの設定

--- dist/basics.js
+++ dist/basics.js
@@ -1,8 +1,5 @@
 "use strict";
 function add(n1, n2, showResult, phrase) {
-    // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
-    //     throw new Error('入力値が正しくありません');
-    // }
     const result = n1 + n2;
     if (showResult) {
         console.log(phrase + result);
