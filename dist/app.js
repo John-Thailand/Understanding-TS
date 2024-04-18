444b2d62914b2db14a75e0b4df8795e8bc088114
John-Thailand <naoki.honda@mirrorfit.jp>
2024-04-18 16:17:24 +0900

    さまざまな関数の定義

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,17 @@
 "use strict";
-console.log('ここにコードを追加します');
+const userName = 'Max';
+let age = 30;
+age = 29;
+const add = (a, b = 1) => {
+    return a + b;
+};
+const printOutput = output => {
+    console.log(output);
+};
+printOutput(add(5));
+const button = document.querySelector('button');
+if (button) {
+    button.addEventListener('click', event => {
+        console.log(event);
+    });
+}
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,28 @@
-console.log('ここにコードを追加します');
+const userName = 'Max';
+let age = 30;
+
+age = 29;
+
+// function add(a: number, b: number) {
+//     let result;
+//     result = a + b;
+//     return result;
+// }
+
+const add = (a: number, b: number = 1) => {
+    return a + b;
+}
+
+const printOutput: (output: string | number) => void = output => {
+    console.log(output);
+}
+
+printOutput(add(5));
+
+const button = document.querySelector('button');
+
+if (button) {
+    button.addEventListener('click', event => {
+        console.log(event);
+    });
+}
