59c7c61a446c7b3f883bcf380b13d07705644334
John-Thailand <naoki.honda@mirrorfit.jp>
2024-04-18 16:47:44 +0900

    配列の便利な機能

--- dist/app.js
+++ dist/app.js
@@ -1,17 +1,26 @@
 "use strict";
-const userName = 'Max';
-let age = 30;
-age = 29;
-const add = (a, b = 1) => {
-    return a + b;
-};
-const printOutput = output => {
-    console.log(output);
-};
-printOutput(add(5));
 const button = document.querySelector('button');
 if (button) {
     button.addEventListener('click', event => {
         console.log(event);
     });
 }
+const hobbies = ['Sports', 'Cooking'];
+const activeHobbies = ['Hiking'];
+activeHobbies.push(...hobbies);
+const person = {
+    firstName: 'Max',
+    age: 30,
+};
+const copiedPerson = Object.assign({}, person);
+const add = (...numbers) => {
+    return numbers.reduce((curResult, curValue) => {
+        return curResult + curValue;
+    }, 0);
+};
+const addedNumbers = add(5, 10, 2, 3.7);
+console.log(addedNumbers);
+const [hobby1, hobby2] = hobbies;
+console.log(hobbies, hobby1, hobby2);
+const { firstName: userName, age } = person;
+console.log(userName, age, person);
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
-const userName = 'Max';
-let age = 30;
+// const userName = 'Max';
+// let age = 30;
 
-age = 29;
+// age = 29;
 
 // function add(a: number, b: number) {
 //     let result;
@@ -9,15 +9,15 @@ age = 29;
 //     return result;
 // }
 
-const add = (a: number, b: number = 1) => {
-    return a + b;
-}
+// const add = (a: number, b: number = 1) => {
+//     return a + b;
+// }
 
-const printOutput: (output: string | number) => void = output => {
-    console.log(output);
-}
+// const printOutput: (output: string | number) => void = output => {
+//     console.log(output);
+// }
 
-printOutput(add(5));
+// printOutput(add(5));
 
 const button = document.querySelector('button');
 
@@ -26,3 +26,37 @@ if (button) {
         console.log(event);
     });
 }
+
+const hobbies = ['Sports', 'Cooking'];
+const activeHobbies = ['Hiking'];
+
+activeHobbies.push(...hobbies);
+
+const person = {
+    firstName: 'Max',
+    age: 30,
+}
+
+const copiedPerson = {
+    ...person,
+};
+
+const add = (...numbers: number[]) => {
+    return numbers.reduce((curResult, curValue) => {
+        return curResult + curValue;
+    }, 0);
+};
+
+const addedNumbers = add(5, 10, 2, 3.7);
+console.log(addedNumbers);
+
+// const hobby1 = hobbies[0];
+// const hobby2 = hobbies[1];
+
+const [hobby1, hobby2] = hobbies;
+console.log(hobbies, hobby1, hobby2);
+
+const {
+    firstName: userName, age
+} = person;
+console.log(userName, age, person);
